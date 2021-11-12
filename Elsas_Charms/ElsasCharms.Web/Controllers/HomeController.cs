c6a606ac6c5ddbcda9e42f624bdae86f8710fab8
Seberiano Castillo <Seberiano Castillo@SEBERIANO>
2021-11-11 20:23:32 -0600

    Aplicar un template para el sitio web público.
    1) Aplicar un template al sitio web público
    
    2) Aplicar los filtros necesarios para presentar la información en el sitio web público.

--- Elsas_Charms/ElsasCharms.Web/Controllers/HomeController.cs
+++ Elsas_Charms/ElsasCharms.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ElsasCharms.BL;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +13,12 @@ namespace ElsasCharms.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var _productosBL = new ProductosBL();
+            var listadeProductos = _productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl = ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+            return View(listadeProductos);
         }
     }
 }
